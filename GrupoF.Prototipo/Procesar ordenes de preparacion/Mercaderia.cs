3863a40eb03aaabd728b69b9e8eb22eb6f7079c7
Diego9n <diegocarlos92.dc@gmail.com>
2024-10-05 23:19:16 -0300

    cambios menores

---
+++ GrupoF.Prototipo/Procesar ordenes de preparacion/Mercaderia.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrupoF.Prototipo.Procesar_ordenes_de_preparacion
+{
+    internal class Mercaderia
+    {
+        public string Descripcion_Mercaderia { get; set; }  
+        public 
+    }
+}
---
+++ GrupoF.Prototipo/Procesar ordenes de preparacion/Transportista.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrupoF.Prototipo.Procesar_ordenes_de_preparacion
+{
+    internal class Transportista
+    {
+        public int Dni_Transportista { get; set; }
+        public string NombreApellido_Transportista {get;set }
+    }
+}

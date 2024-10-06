0c09bc5e9893ce4321ea4a66ce54d15bb8f820b4
Agustin Tallone <58TA42102237@campus.economicas.uba.ar>
2024-10-06 13:40:40 -0300

    Se agrega carpeta Readme

---
+++ GrupoF.Prototipo/README/README.cs
@@ -0,0 +1,13 @@
+﻿/*
+Estimado,
+
+Se detallan a continuacion las cuestiones tecnicas que se tuvieron en cuenta:
+
+-Las pantallas no tienen relacionan entre si, ninguna sabe de la existencia de la otra.
+-Cada pantalla cuenta con su carpeta, que incluye formulario, modelo y clases.
+-Las clases no se comparten entre pantallas.
+-El form controla la logica de la interfaz grafica, mientras que el modelo contiene la logica del negocio.
+-Los elementos graficos tienen sus nombres modificados para mayor organizacion.
+-El formulario sabe cual es el modelo, pero el modelo no sabe cual es formulario.
+-El formulario no cuenta con datos.
+*/

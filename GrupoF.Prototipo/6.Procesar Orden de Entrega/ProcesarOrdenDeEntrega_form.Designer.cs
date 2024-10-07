9e7a55a777a2f7dc739780e3e6b45e296862d5dc
Agustin Tallone <58TA42102237@campus.economicas.uba.ar>
2024-10-06 23:14:14 -0300

    Pantalla 6

--- GrupoF.Prototipo/6.Procesar Orden de Entrega/ProcesarOrdenDeEntrega_form.Designer.cs
+++ GrupoF.Prototipo/6.Procesar Orden de Entrega/ProcesarOrdenDeEntrega_form.Designer.cs
@@ -36,7 +36,7 @@
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
             Name = "ProcesarOrdenDeEntrega_form";
-            Text = "ProcesarOrdenDeEntrega_form";
+            Text = "Procesar Orden De Entrega";
             ResumeLayout(false);
         }
 

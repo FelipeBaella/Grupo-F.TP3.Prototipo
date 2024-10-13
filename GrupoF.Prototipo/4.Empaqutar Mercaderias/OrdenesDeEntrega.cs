276a67b66358ca0271d2819ef2d9939a8cde68cb
QuimeyLopez <qlopez@intermaco.com.ar>
2024-10-13 14:54:02 -0300

    Cambios remito

--- GrupoF.Prototipo/4.Empaqutar Mercaderias/OrdenesDeEntrega.cs
+++ GrupoF.Prototipo/4.Empaqutar Mercaderias/OrdenesDeEntrega.cs
@@ -9,8 +9,8 @@ namespace GrupoF.Prototipo._3.Procesar_Orden_de_Seleccion
     internal class OrdenesDeEntrega
     {
         public int Id_OrdenDeEntrega { get; set; }
-        public int Id_Estado{ get; set; }
         public int Id_OrdenDePreparacion { get; set; }
         public DateTime Emision_OrdenDeEntrega { get; set; }
+        public bool Procesado_OrdenDeEntrega { get; set; }
     }
 }
---
+++ GrupoF.Prototipo/6.Despachar Mercaderias/Remito.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrupoF.Prototipo._3.Procesar_Orden_de_Seleccion
+{
+    internal class Remito
+    {
+        public int Id_Remito { get; set; }
+        public DateTime Emision_Remito { get; set; }
+        public bool Procesado_Remito { get; set; }
+    }
+}
